02e0d56ec8eb63f853d2561b52400effe1276eb8
Blinova-Olga <61988920+Blinova-Olga@users.noreply.github.com>
2020-05-29 16:21:14 +0800

    Add files via upload

---
+++ ToDoList/Kalendar_Console/Header.h
@@ -0,0 +1,47 @@
+#pragma once
+#define MAX_NAME_SIZE 40 // Ширина колонки с именем
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+string weekday(int day);
+
+class Case {
+public:
+    Case() {
+        priority = 0;
+        name = "";
+        done = false;
+        SetTime(time(NULL)); // По умолчанию, срок задаётся = времени создания объекта
+        SetRepeat(0); // По умолчанию не повторять
+    } // Конструктор
+
+    tm when;// Временная структура = срок
+
+    bool done; // Маркер завершённости задачи
+
+    tm repeat; //Повтор через. часов (+8) / дней (+1) / месяцев / лет (+70)
+
+    int priority; // Приоритет задачи, 0 - нейтральная, 1 - важная, 2 - очень важная
+
+    string name; // Строка, содержащая само название задачи
+      
+    void Write(ofstream& f); // Запись в конец файла
+      
+    void Ask(); // Заполнение пользователем. Перед ним обязательно нужно сделать SetTime !!!!
+
+    void Print(int k);
+
+    void SetTime(const time_t now) {
+        localtime_s(&when, &now); // Заполнение временной структуры tm по времени в секндах
+    }
+    // Заполнение полей времени
+    void SetRepeat(const time_t now) {
+        localtime_s(&repeat, &now);
+    }
+};
